85876b5959edef3a32768d165c86212c2c997199
Isadora dos Santos Ribeiro <isadora.ribeiro@automind.com.br>
2023-10-19 16:48:39 -0300

    new files

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -2,31 +2,77 @@
 
 describe('Should test at a backend level', () => {
     before(() => {
-
+       
     })
 
-    it('Should create an account', () => {
+    it('Cadastrar Usuario', () => {
         cy.request({
             method: 'POST',
-            url: 'https://seubarriga.wcaquino.me/logar',
+            url: 'https://serverest.dev/usuarios',
             body:{
-                email: 'isa@santos.com',
-                senha: 'isalinda14'
-            }
-        })
-        // }).then(res => console.log(res))
-
-        cy.request({
-            url:'https://seubarriga.wcaquino.me/salvarConta',
-            method: 'POST',
-            body:{
-                nome: 'uma conta'
+                nome: 'nome',
+                email: 'fulano@126.com.br',
+                password: '123456',
+                administrador: 'true'
             }
         }).as('response')
-            cy.get('@response').then(res => {
+            cy.get('@response').then(res =>{
+                expect(res.status).to.be.equal(201)
+                expect(res.body).to.have.property('message', 'Cadastro realizado com sucesso')
+           })
+        })
+           it('Usuário ja cadastrado ', () => {
+            cy.request({
+                method: 'POST',
+                url: 'https://serverest.dev/usuarios',
+                body:{
+                    nome: 'teste',
+                    email: 'isa@santos.com.br',
+                    password: '123456',
+                    administrador: 'true'
+                }
+            }).as('response')
+                cy.get('@response').then(res =>{
+                    expect(res.status).to.be.equal(400)
+                    expect(res.body).to.have.property('message', 'Este email já está sendo usado')
+               })
+        })  
+         it('Login', () => {
+            cy.request({
+                method: 'POST',
+                url:'https://serverest.dev/login',
+                body :{
+                email: 'isa@santos.com.br',
+                password: "123456"       
+                }
+            }).as('response')
+            cy.get('@response').then(res =>{ 
                 expect(res.status).to.be.equal(200)
             })
         })
-    // }).then(res => console.log(res))
+        // it('Listar um usuário cadastrado', () => {
+        //     cy.request({
+        //         method: 'GET',
+        //         url: 'https://serverest.dev/usuarios?',
+        //         body:{}
+        //     })
+        // })
+        // it('Buscar usuário por ID', () => {
+        //     cy.request({
+        //         method:'GET'
+        //     })
 
+        // })
+        // it('Excluir usuário', () => {
+        //     cy.request({
+        //         method: 'DELETE'
+        //     })
+
+        // })
+        // it('Editar usuário', () => {
+
+       
+    
 })
+        
+    
